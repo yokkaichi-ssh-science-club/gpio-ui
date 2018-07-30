25d0d36cb5f4dd9a7c3de91ba28ac20b398f246e
takeda5955 <41512077+takeda5955@users.noreply.github.com>
2018-07-30 14:22:21 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ require('./scss/index.scss')
 require('babel-polyfill')
 const Vue = require("vue/dist/vue.runtime.min")
 const VueOnsen = require('vue-onsenui')
-const VueI18n=require("vue-i18n")
 const Vuex = require("vuex")
 
 Vue.use(VueOnsen)
 Vue.use(Vuex)
-Vue.use(VueI18n)
 
 Vue.component('custom-bar', require("./component/customBar"))
 
@@ -34,9 +32,5 @@ exports.vm= new Vue({
       }
       html.setAttribute('onsflag-iphonex-landscape', '');
     }
-  },
-  i18n: new VueI18n.default({
-    locale: 'ja', // set locale
-    messages:require("./i18n.json"), // set locale messages
-  })
+  }
 })
